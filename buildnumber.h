7da333a5f729e893d512871a5072a9ce661aca05
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2021-04-28 22:07:50 +0200

    1

--- buildnumber.h
+++ buildnumber.h
@@ -17,3 +17,14 @@ struct Buildnumber{ static QString toString(); };
 QString Buildnumber::toString(){return QString::number(BUILDNUMBER);}
 */
 
+//buildnum_insole.sh
+/*
+#!/bin/bash
+
+LD_LIBRARY_PATH=/home/zoli/commonlib_debug_64/:/home/zoli/Qt/5.9/gcc_64/lib:/home/zoli/Qt/5.9/gcc_64/lib
+TEMPLATE=~/buildnumber.h.tmp
+OUTPUTFILE=~/insole1/buildnumber.h
+PROJECT=pizero_mcp3008_api
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./buildnum_ -t $TEMPLATE -o $OUTPUTFILE -p $PROJECT
+*/
