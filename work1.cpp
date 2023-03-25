a3127a68a704d8935e2d18c315782b1005f9241b
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2023-03-25 22:43:57 +0100

    1

--- work1.cpp
+++ work1.cpp
@@ -2,13 +2,37 @@
 #include "helpers/logger.h"
 #include "helpers/sqlhelper.h"
 #include "helpers/textfilehelper.h"
+#include "qregularexpression.h"
 #include "settings.h"
 #include "environment.h"
+#include <QCoreApplication>
+#include <QRegularExpression>
 #include <QVariant>
 #include <iostream>
 
 extern Settings _settings;
 extern Environment _environment;
+
+const QRegularExpression Work1::_regexp1(R"(#define\s+BUILDNUMBER\s+[\w\S]*)");
+const QString Work1::_template(R"(#ifndef BUILDNUMBER_H
+#define BUILDNUMBER_H
+
+#define BUILDNUMBER 1022
+
+#include <QString>
+
+struct Buildnumber{ static QString toString(); };
+
+#endif // BUILDNUMBER_H
+
+/*
+// buildnumber.cpp should contain this lines below to forcing build with makefile depencencies
+#include <QString>
+#include "buildnumber.h"
+
+QString Buildnumber::toString(){return QString::number(BUILDNUMBER);}
+*/
+)");
 
 Work1Params Work1::params;
 
@@ -26,31 +50,65 @@ auto Work1::doWork() -> int
     auto buildnum_str = QString::number(buildnum);
     std::cout << buildnum_str.toStdString() << '\n';
     zInfo(QStringLiteral("%1:%2").arg(params.projname).arg(buildnum));
-    if(!params.tmpfile.endsWith(".tmp")) return 4;
-    if(params.tmpfile.isEmpty()) return 4;
-    QString a;
-    bool loaded = TextFileHelper::Load(params.tmpfile, &a);
-    if(!loaded){
-        zInfo(QStringLiteral("load failed: ")+params.tmpfile);
-        return 4;
-    }
 
-    if(a.isEmpty()) return 5;
+    bool tempFile = !params.tmpfile.isEmpty();
+    if(tempFile){
+        if(params.tmpfile.endsWith(".tmp"))
+        {
+            BuildNum(params.tmpfile, params.ofile, buildnum_str);
+        } else {
+            BuildNum2(params.tmpfile, buildnum_str);
+        }
+    }
+    return 0;
+}
+
+bool Work1::BuildNum(const QString& tmpFile, const QString& outputFile, const QString& buildnum_str){
+    //if(!tmpFile.endsWith(".tmp")) return 4;
+    QString a;
+
+    bool loaded = TextFileHelper::Load(tmpFile, &a);
+    if(!loaded){
+        zInfo(QStringLiteral("load failed: ")+TextFileHelper::LastError());
+        return false;
+    }
+    if(a.isEmpty()) return false;
     a = a.replace("${BUILDNUMBER}", buildnum_str);
-    auto fn2 = params.tmpfile.left(params.tmpfile.length()-4);
+    auto fn2 = tmpFile.left(tmpFile.length()-4);
     QString of="";
-    if(params.ofile.isEmpty())
+    if(outputFile.isEmpty())
         of = QFileInfo(fn2).fileName();
     else
-        of = params.ofile;
+        of = outputFile;
 
     bool saved = TextFileHelper::Save(a, of);
     if(!saved){
         zInfo(QStringLiteral("save failed: ")+of)
-        return 4;
+        return false;
+    }
+    return true;
+}
+
+bool Work1::BuildNum2(const QString& tmpFile, const QString& buildnum_str){
+    QString a;
+    bool loaded = TextFileHelper::Load(tmpFile, &a);
+    if(!loaded){
+        zInfo(QStringLiteral("load failed: ")+TextFileHelper::LastError());
+        a = _template;
+    } else{
+        if(a.isEmpty()) a = _template;
     }
 
-    return 0;
+    auto match = _regexp1.match(a);
+    if(!match.hasMatch()) return false;
+    a.replace(match.capturedStart(), match.capturedLength(), "#define BUILDNUMBER "+buildnum_str);
+
+    bool saved = TextFileHelper::Save(a, tmpFile);
+    if(!saved){
+        zInfo(QStringLiteral("save failed: ")+TextFileHelper::LastError());
+        return false;
+    }
+    return true;
 }
 
 
--- work1.h
+++ work1.h
@@ -17,7 +17,14 @@ public:
     Work1();
     static int doWork();
     static Work1Params params;
+    static bool BuildNum(const QString &params_tmpfile,
+                         const QString &params_ofile,
+                         const QString &buildnum_str);
+    static bool BuildNum2(const QString &params_tmpfile,
+                         const QString &buildnum_str);
 private:
+    static const QString _template;
+    static const QRegularExpression _regexp1;
     static int getBuildNum(const QString& conn, int *b);
 };
 
